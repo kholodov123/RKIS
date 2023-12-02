18e594fc7ab0a3645b2fe451e33db0b7dde5a89d
aleksandr <hggg47122@gmail.com>
2023-12-02 15:12:11 +0300

    commit

--- WebBooks/WebBooks/settings.py
+++ WebBooks/WebBooks/settings.py
@@ -19,10 +19,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-w1h2om!=ae!2u7ht$vo=gh9v017)ku*06@p^h7x6gmq)j216g+'
+# SECRET_KEY = 'dhp40_!05cp071e9pd5e5+3_90fev*vq-obx^3^hv8cx0=l#!k'
+
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
 ALLOWED_HOSTS = []
 
